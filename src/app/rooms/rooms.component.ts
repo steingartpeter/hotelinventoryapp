44ccecdab1a580b18bd3800e707ce27057cf7c27
steingartpeter <steingart.p2@gmail.com>
2023-01-12 13:34:58 +0100

    2023-01-12 001

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Room, RoomList } from './rooms';
+import hu from '@angular/common/locales/hu';
+import { registerLocaleData } from '@angular/common';
 
 @Component({
   selector: 'hinv-rooms',
@@ -22,31 +24,39 @@ export class RoomsComponent implements OnInit {
       roomType: 'Deluxe Room',
       amenities: 'Air Conditioner, Free Wi-Fi,TV, Bathroom, Kitchen',
       price: 500,
-      photos: 'https://images.unsplach.com/phozo-1518761841217-8f162f1e1131',
+      photos:
+        'https://www.italianbark.com/wp-content/uploads/2018/01/hotel-room-design-trends-italianbark-interior-design-blog.jpg',
       checkinTime: new Date('2022-12-01'),
       checkoutTime: new Date('2023-01-11'),
+      rating: 4.5,
     },
     {
       roomNumber: 2,
       roomType: 'Deluxe Room Upper',
       amenities: 'Air Conditioner, Free Wi-Fi,TV, Bathroom, Kitchen',
       price: 1000,
-      photos: 'https://images.unsplach.com/phozo-1518761841217-8f162f1e1131',
+      photos:
+        'https://media.architecturaldigest.com/photos/56c64d885ef3a2f746a41ffb/16:9/w_2580,c_limit/meyer-davis-guest-room-tips-01.jpg',
       checkinTime: new Date('2022-12-31'),
       checkoutTime: new Date('2023-01-10'),
+      rating: 3.4,
     },
     {
       roomNumber: 3,
       roomType: 'Private Suit',
       amenities: 'Air Conditioner, Free Wi-Fi,TV, Bathroom, Kitchen',
       price: 1500,
-      photos: 'https://images.unsplach.com/phozo-1518761841217-8f162f1e1131',
+      photos:
+        'https://info.ehl.edu/hubfs/Imported_Blog_Media/Hospitality_Insights_Hotel_Design_Trends.jpg',
       checkinTime: new Date('2022-12-31'),
       checkoutTime: new Date('2023-01-10'),
+      rating: 2.6,
     },
   ];
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    registerLocaleData(hu);
+  }
   toggle() {
     this.hideRooms = !this.hideRooms;
   }
